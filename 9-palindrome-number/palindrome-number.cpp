fdd9810408db6e31d9b29cf305f7582287bbb3e2
Yogesh kumar meena <yogeshchhandhwal2005@gmail.com>
2026-07-13 23:13:26 +0530

    Time: 3 ms (44.80%) | Memory: 8.5 MB (92.74%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative or ends with 0 (but not 0 itself)
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even digits: x == reversedHalf
+        // For odd digits: x == reversedHalf/10 (middle digit ignored)
+        return (x == reversedHalf || x == reversedHalf / 10);
+    }
+};
