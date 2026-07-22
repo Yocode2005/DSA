54f27f4a7ee189dfee622d64440b4c75b50d9daf
Yogesh kumar meena <yogeshchhandhwal2005@gmail.com>
2026-07-22 23:15:54 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (16.29%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,54 @@
+// class Solution {
+// public:
+
+//     long long maxTree(TreeNode* root){
+
+//         if(root == NULL) return LLONG_MIN;
+
+//         return max((long long)root->val,
+//                    max(maxTree(root->left),
+//                        maxTree(root->right)));
+//     }
+
+//     long long minTree(TreeNode* root){
+
+//         if(root == NULL) return LLONG_MAX;
+
+//         return min((long long)root->val,
+//                    min(minTree(root->left),
+//                        minTree(root->right)));
+//     }
+
+//     bool isValidBST(TreeNode* root) {
+
+//         if(root == NULL) return true;
+
+//         if(root->val <= maxTree(root->left))
+//             return false;
+
+//         if(root->val >= minTree(root->right))
+//             return false;
+
+//         return isValidBST(root->left) &&
+//                isValidBST(root->right);
+//     }
+// };
+
+
+class Solution {
+    public:
+    void inorder(TreeNode* root, vector<int> & ans){
+        if(root == NULL) return;
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
+    bool isValidBST(TreeNode* root){
+        vector<int> ans;
+        inorder(root,ans);
+        for(int i = 1;i<ans.size();i++){
+            if(ans[i]<=ans[i-1]) return false;
+        }
+        return true;
+    }
+};
