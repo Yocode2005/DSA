db3df71ca9974652f96342260170d3d3409a10bb
Yogesh kumar meena <yogeshchhandhwal2005@gmail.com>
2026-07-23 20:09:16 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (48.90%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+     int jump = 0,l = 0,r = 0;
+     int n = nums.size();
+     while(r<n-1){
+        int longest = 0;
+        for(int i = l;i<=r;i++){
+            longest = max(longest,i + nums[i]);
+        }
+        l = r+1;
+        r = longest;
+        jump = jump+1;
+     }
+     return jump;
+    }
+};
